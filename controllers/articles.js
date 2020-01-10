f44b49dbaf08dad3beeb694a2dbd02890b7f3f08
hayk77 <hayksimonyan7@gmail.com>
2020-01-10 11:20:36 +0400

    added navbar routes with $regex based on title

--- controllers/articles.js
+++ controllers/articles.js
@@ -36,6 +36,186 @@ exports.getIndex = (req, res, next) => {
         });
 };
 
+exports.getTech = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalArticles;
+
+    Article.find({title: { $regex: '.*' + 'tech' + '.*' } })
+        .countDocuments()
+        .then(numArticles => {
+            totalArticles = numArticles;
+            return Article.find({title: { $regex: '.*' + 'tech' + '.*' } })
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE);
+        })
+        .then(allArticles => {
+            res.render('articles/index', { 
+                allArticles: allArticles,
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalArticles,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalArticles / ITEMS_PER_PAGE)
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
+exports.getMath = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalArticles;
+
+    Article.find({title: { $regex: '.*' + 'math' + '.*', $regex: '.*' + 'Math' + '.*' } })
+        .countDocuments()
+        .then(numArticles => {
+            totalArticles = numArticles;
+            return Article.find({title: { $regex: '.*' + 'math' + '.*', $regex: '.*' + 'Math' + '.*' } })
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE);
+        })
+        .then(allArticles => {
+            res.render('articles/index', { 
+                allArticles: allArticles,
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalArticles,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalArticles / ITEMS_PER_PAGE)
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
+exports.getPhysics = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalArticles;
+
+    Article.find({title: { $regex: '.*' + 'physics' + '.*' } })
+        .countDocuments()
+        .then(numArticles => {
+            totalArticles = numArticles;
+            return Article.find({title: { $regex: '.*' + 'physics' + '.*' } })
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE);
+        })
+        .then(allArticles => {
+            res.render('articles/index', { 
+                allArticles: allArticles,
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalArticles,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalArticles / ITEMS_PER_PAGE)
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
+exports.getProgramming = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalArticles;
+
+    Article.find({title: { $regex: '.*' + 'programming' + '.*' } })
+        .countDocuments()
+        .then(numArticles => {
+            totalArticles = numArticles;
+            return Article.find({title: { $regex: '.*' + 'programming' + '.*' } })
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE);
+        })
+        .then(allArticles => {
+            res.render('articles/index', { 
+                allArticles: allArticles,
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalArticles,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalArticles / ITEMS_PER_PAGE)
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
+exports.getEngineering = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalArticles;
+
+    Article.find({title: { $regex: '.*' + 'engineering' + '.*' } })
+        .countDocuments()
+        .then(numArticles => {
+            totalArticles = numArticles;
+            return Article.find({title: { $regex: '.*' + 'engineering' + '.*' } })
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE);
+        })
+        .then(allArticles => {
+            res.render('articles/index', { 
+                allArticles: allArticles,
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalArticles,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalArticles / ITEMS_PER_PAGE)
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
+exports.getEnergetics = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalArticles;
+
+    Article.find({title: { $regex: '.*' + 'energetics' + '.*' } })
+        .countDocuments()
+        .then(numArticles => {
+            totalArticles = numArticles;
+            return Article.find({title: { $regex: '.*' + 'energetics' + '.*' } })
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE);
+        })
+        .then(allArticles => {
+            res.render('articles/index', { 
+                allArticles: allArticles,
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalArticles,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalArticles / ITEMS_PER_PAGE)
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
 exports.getShow = (req, res, next) => {
     Article.findById(req.params.id)
         .populate('comments')
--- routes/articles.js
+++ routes/articles.js
@@ -8,10 +8,34 @@ router.route('/')
 
 router.route('/articles')
   // INDEX
-  .get(articlesController.getIndex)
+  .get(articlesController.getIndex);
+  
+router.route('/articles/tech')
+  // INDEX
+  .get(articlesController.getTech);
+  
+router.route('/articles/math')
+  // INDEX
+  .get(articlesController.getMath);
+  
+router.route('/articles/physics')
+  // INDEX
+  .get(articlesController.getPhysics);
+
+router.route('/articles/programming')
+  // INDEX
+  .get(articlesController.getProgramming);
+
+router.route('/articles/engineering')
+  // INDEX
+  .get(articlesController.getEngineering);
+
+router.route('/articles/energetics')
+  // INDEX
+  .get(articlesController.getEnergetics);
 
 router.route('/articles/:id')
   // SHOW
-  .get(articlesController.getShow)
+  .get(articlesController.getShow);
 
 module.exports = router;

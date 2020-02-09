308d124ad460e0365117aab7fec779f595ca3e59
hayk77 <hayksimonyan7@gmail.com>
2020-02-09 14:17:19 +0400

    modified navbar styling, added dynamic category routes

--- routes/articles.js
+++ routes/articles.js
@@ -2,39 +2,22 @@ const express = require('express');
 const router = express.Router();
 const articlesController = require('../controllers/articles');
 
-router.route('/')
+router
+  .route('/')
   /* GET home page. */
   .get(articlesController.getHomePage);
 
-router.route('/articles')
+router
+  .route('/articles')
   // INDEX
   .get(articlesController.getIndex);
-  
-router.route('/articles/tech')
-  // INDEX
-  .get(articlesController.getTech);
-  
-router.route('/articles/math')
-  // INDEX
-  .get(articlesController.getMath);
-  
-router.route('/articles/physics')
-  // INDEX
-  .get(articlesController.getPhysics);
 
-router.route('/articles/programming')
-  // INDEX
-  .get(articlesController.getProgramming);
+router
+  .route('/articles/category/:categoryName')
+  .get(articlesController.getCategory);
 
-router.route('/articles/engineering')
-  // INDEX
-  .get(articlesController.getEngineering);
-
-router.route('/articles/energetics')
-  // INDEX
-  .get(articlesController.getEnergetics);
-
-router.route('/articles/:id')
+router
+  .route('/articles/:id')
   // SHOW
   .get(articlesController.getShow);
 

bf42cc6e546f46e9ab82ba62d6b97d2603443344
hayk77 <hayksimonyan7@gmail.com>
2020-02-09 14:41:16 +0400

    connected to mongodb atlas, styled sign in and sign up pages

--- util/database.js
+++ util/database.js
@@ -2,23 +2,24 @@ const mongoose = require('mongoose');
 //require chalk module to give colors to console text
 const chalk = require('chalk');
 
-const dbURL = 'mongodb://localhost/blog';
+const dbURL =
+  'mongodb+srv://s1:s1@cluster0-bq6u5.mongodb.net/test?retryWrites=true&w=majority';
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    autoIndex: false, // Don't build indexes
-    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0,
-    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4, // Use IPv4, skip trying IPv6
-    // keepAlive: true, 
-    // keepAliveInitialDelay: 300000
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0,
+  connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4 // Use IPv4, skip trying IPv6
+  // keepAlive: true,
+  // keepAliveInitialDelay: 300000
 };
 
 const connected = chalk.bold.cyan;
@@ -28,24 +29,30 @@ const termination = chalk.bold.magenta;
 
 //export this function and imported by server.js
 module.exports = () => {
-    mongoose.connect(dbURL, options);
+  mongoose.connect(dbURL, options);
 
-    mongoose.connection.on('connected', () => {
-        console.log(connected("Mongoose default connection is open to ", dbURL));
-    });
+  mongoose.connection.on('connected', () => {
+    console.log(connected('Mongoose default connection is open to ', dbURL));
+  });
 
-    mongoose.connection.on('error', err => {
-        console.log(error("Mongoose default connection has occured "+err+" error"));
-    });
+  mongoose.connection.on('error', err => {
+    console.log(
+      error('Mongoose default connection has occured ' + err + ' error')
+    );
+  });
 
-    mongoose.connection.on('disconnected', () => {
-        console.log(disconnected("Mongoose default connection is disconnected"));
-    });
+  mongoose.connection.on('disconnected', () => {
+    console.log(disconnected('Mongoose default connection is disconnected'));
+  });
 
-    process.on('SIGINT', () => {
-        mongoose.connection.close(() => {
-            console.log(termination("Mongoose default connection is disconnected due to application termination"));
-            process.exit(0)
-        });
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log(
+        termination(
+          'Mongoose default connection is disconnected due to application termination'
+        )
+      );
+      process.exit(0);
     });
-}
+  });
+};
